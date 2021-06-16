8bc6b2cf0045bb93a6101cb5a56338d9accb6ab1
amandabriena <amanda.briena@gmail.com>
2021-06-15 23:45:12 -0300

    exclusão e otimização de arquivos e filtro de atividades do dia e do usuário em Minha Rotina

---
+++ app/src/main/java/com/example/meuprojeto/controller/PopupDeletarAtividadeActivity.java
@@ -0,0 +1,14 @@
+package com.example.meuprojeto.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopupDeletarAtividadeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_deletar_atividade);
+    }
+}
