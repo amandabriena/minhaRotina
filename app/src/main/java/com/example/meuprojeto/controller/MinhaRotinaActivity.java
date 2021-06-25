af871aa6b3275dee764f6fbe7c406526d31854d6
amandabriena <amanda.briena@gmail.com>
2021-06-25 00:14:48 -0300

    compartilhamento da rotina do dia

--- app/src/main/java/com/example/meuprojeto/controller/MinhaRotinaActivity.java
+++ app/src/main/java/com/example/meuprojeto/controller/MinhaRotinaActivity.java
@@ -6,11 +6,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.meuprojeto.R;
 import com.example.meuprojeto.model.Atividade;
@@ -22,19 +30,25 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.UUID;
 
 public class MinhaRotinaActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerViewAdapter recyclerViewAdapter;
     private List<Atividade> listaAtividades = new ArrayList<>();
-    private String idUsuario;
     Button btEspacoPais;
+    ImageButton btCompartilhar;
 
 
     @Override
@@ -43,6 +57,7 @@ public class MinhaRotinaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_minha_rotina);
 
         btEspacoPais = (Button) findViewById(R.id.btEspacoPais);
+        btCompartilhar = (ImageButton) findViewById(R.id.btCompartilhar);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerViewAdapter = new RecyclerViewAdapter(listaAtividades);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
@@ -57,7 +72,6 @@ public class MinhaRotinaActivity extends AppCompatActivity {
                 intent.putExtra("atividade", atividade);
                 intent.putExtra("idAtividade", atividade.getId());
                 startActivity(intent);
-                //Toast.makeText(MinhaRotinaActivity.this, atividade.getId(), Toast.LENGTH_LONG).show();
             }
         });
         recyclerView.setAdapter(recyclerViewAdapter);
@@ -71,6 +85,64 @@ public class MinhaRotinaActivity extends AppCompatActivity {
                 startActivity(pais);
             }
         });
+        btCompartilhar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                screenShot(recyclerView);
+            }
+        });
+
+    }
+    public void screenShot(RecyclerView view) {
+
+        /*String fileName = Environment.getExternalStorageDirectory() + "/screenshot.jpg";
+        View root = getWindow().getDecorView();
+        root.setDrawingCacheEnabled(true);
+        root.buildDrawingCache(true);
+        Bitmap bitmap = Bitmap.createBitmap(root.getDrawingCache());
+        root.setDrawingCacheEnabled(false);
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "Title", null);
+        Uri uriImagem = Uri.parse(path);*/
+        int size = view.getAdapter().getItemCount();
+        RecyclerView.ViewHolder holder = view.getAdapter().createViewHolder(view, 0);
+        view.getAdapter().onBindViewHolder(holder, 0);
+        holder.itemView.measure(View.MeasureSpec.makeMeasureSpec(view.getWidth(), View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        holder.itemView.layout(0, 0, holder.itemView.getMeasuredWidth(), holder.itemView.getMeasuredHeight());
+        Bitmap bigBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), holder.itemView.getMeasuredHeight() * size,
+                Bitmap.Config.ARGB_8888);
+        Canvas bigCanvas = new Canvas(bigBitmap);
+        bigCanvas.drawColor(Color.WHITE);
+        Paint paint = new Paint();
+        int iHeight = 0;
+        holder.itemView.setDrawingCacheEnabled(true);
+        holder.itemView.buildDrawingCache();
+        bigCanvas.drawBitmap(holder.itemView.getDrawingCache(), 0f, iHeight, paint);
+        holder.itemView.setDrawingCacheEnabled(false);
+        holder.itemView.destroyDrawingCache();
+        iHeight += holder.itemView.getMeasuredHeight();
+        for (int i = 1; i < size; i++) {
+            view.getAdapter().onBindViewHolder(holder, i);
+            holder.itemView.setDrawingCacheEnabled(true);
+            holder.itemView.buildDrawingCache();
+            bigCanvas.drawBitmap(holder.itemView.getDrawingCache(), 0f, iHeight, paint);
+            iHeight += holder.itemView.getMeasuredHeight();
+            holder.itemView.setDrawingCacheEnabled(false);
+            holder.itemView.destroyDrawingCache();
+        }
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bigBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(), bigBitmap, "Title", null);
+        Uri uriImagem = Uri.parse(path);
+        Log.e("URI IMAGEM", uriImagem.toString());
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "Minha Rotina de Hoje");
+        intent.putExtra(Intent.EXTRA_STREAM, uriImagem);
+        //intent.setType("image/*");
+        intent.setType("*/*");
+        startActivity(Intent.createChooser(intent, "Share Image"));
     }
     public class CarregarListaAsynctask extends AsyncTask<Void, Void, Void> {
 
