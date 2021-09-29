4b07a0d630d11c60f801b31d444ff82c318093f2
amandabriena <amanda.briena@gmail.com>
2021-09-28 23:22:12 -0300

    inclusão de atividade e evento no calendário de atvs agendadas

--- app/src/main/java/com/example/meuprojeto/controller/AtividadesAgendadasActivity.java
+++ app/src/main/java/com/example/meuprojeto/controller/AtividadesAgendadasActivity.java
@@ -1,8 +1,10 @@
 package com.example.meuprojeto.controller;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.CalendarView;
@@ -10,8 +12,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.meuprojeto.R;
+import com.example.meuprojeto.model.Atividade;
+import com.example.meuprojeto.model.AtividadeAgendada;
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.type.Color;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
@@ -21,11 +32,14 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.Locale;
 
+import static com.example.meuprojeto.util.Utilitarios.verificarDiaSemana;
 import static java.util.Calendar.DAY_OF_MONTH;
 
 public class AtividadesAgendadasActivity extends AppCompatActivity {
@@ -34,6 +48,7 @@ public class AtividadesAgendadasActivity extends AppCompatActivity {
     CompactCalendarView compactCalendar;
     private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM yyyy", Locale.getDefault()) ;
     private TextView mes;
+    private List<AtividadeAgendada> listaAtividades = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +57,8 @@ public class AtividadesAgendadasActivity extends AppCompatActivity {
         mes = (TextView) findViewById(R.id.mes);
         compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
         compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        new CarregarListaAsynctask().execute();
 
         //Setando informação do mês atual
         Date d = new Date();
@@ -60,11 +77,10 @@ public class AtividadesAgendadasActivity extends AppCompatActivity {
         compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
             @Override
             public void onDayClick(Date dateClicked) {
-                Log.e("Alarme", "Date: "+cal.getTime())
-                if(dateClicked.compareTo(cal.getTime())== 1){
-                    Toast.makeText(AtividadesAgendadasActivity.this, "DIA D", Toast.LENGTH_SHORT).show();
-                }
-
+                List<Event> events = compactCalendar.getEvents(dateClicked);
+                //Log.e("Alarme", "Date: "+cal.getTime());
+                Log.e("Alarme", "Day was clicked: " + dateClicked + " with events " + events);
+                
             }
 
             @Override
@@ -74,5 +90,45 @@ public class AtividadesAgendadasActivity extends AppCompatActivity {
         });
 
 
+    }
+    public class CarregarListaAsynctask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // carregar do banco
+            Log.e("Alarme", "carregando atividades..");
+            final String dia = verificarDiaSemana();
+            FirebaseFirestore.getInstance().collection("usuarios")
+                    .document(FirebaseAuth.getInstance().getUid()).collection("atividadesAgendadas")
+                    .orderBy("data", Query.Direction.ASCENDING)
+                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                            if(error != null){
+                                Log.e("Erro", error.getMessage());
+                                return;
+                            }
+                            List<DocumentSnapshot> docs = value.getDocuments();
+                            for(DocumentSnapshot doc : docs){
+                                AtividadeAgendada atv = doc.toObject(AtividadeAgendada.class);
+                                listaAtividades.add(atv);
+                                incluirEvento(atv);
+                                Log.e("Alarme", "Atividade Agendada: "+atv.getNomeAtividade());
+
+                            }
+                        }
+                    });
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void resultado) {
+        }
+    }
+
+    public void incluirEvento(AtividadeAgendada atv){
+        Event evento = new Event(R.color.colorPrimary, atv.getData().getTime(), atv.getNomeAtividade());
+        compactCalendar.addEvent(evento);
+        Log.e("Alarme", "Evento add: "+atv.getNomeAtividade());
+        Log.e("Alarme", "Evento time: "+evento.getTimeInMillis());
     }
 }
