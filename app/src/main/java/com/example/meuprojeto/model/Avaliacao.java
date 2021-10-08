0c74fbb5831dfc252d0f298738adf2703d2364f7
amandabriena <amanda.briena@gmail.com>
2021-10-07 23:35:38 -0300

    feat: crianção finalizada da classe avaliação

--- app/src/main/java/com/example/meuprojeto/model/Avaliacao.java
+++ app/src/main/java/com/example/meuprojeto/model/Avaliacao.java
@@ -5,4 +5,33 @@ public class Avaliacao {
     private int avaliacao;
     private String id_atividade;
 
+    public Avaliacao(String id, int avaliacao, String id_atividade) {
+        this.id = id;
+        this.avaliacao = avaliacao;
+        this.id_atividade = id_atividade;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public String getId_atividade() {
+        return id_atividade;
+    }
+
+    public void setId_atividade(String id_atividade) {
+        this.id_atividade = id_atividade;
+    }
 }
